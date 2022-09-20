f62c13ca9c2c0483a248340d175c9db14f09a9b9
Psairohith <112462595+Psairohith@users.noreply.github.com>
2022-09-20 09:40:47 +0530

    Add files via upload

---
+++ reverse order.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+string str="temple";
+string str="sign up";
+string str="at-least";
+string str="1245";
+string str="!@#$%";
+string str="145*999=144855";
+reverse(str.begin(),str.end());
+cout<<str;
+return 0;
+}
